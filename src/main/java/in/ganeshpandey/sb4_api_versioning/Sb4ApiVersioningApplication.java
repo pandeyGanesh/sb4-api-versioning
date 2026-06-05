9a4ed99adf6889ef413377646e8e7533c4671dd0
pandeyGanesh <pandey.ganesh12@gmail.com>
2026-06-05 22:56:00 +0530

    Initial setup

---
+++ src/main/java/in/ganeshpandey/sb4_api_versioning/Sb4ApiVersioningApplication.java
@@ -0,0 +1,13 @@
+package in.ganeshpandey.sb4_api_versioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb4ApiVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb4ApiVersioningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ganeshpandey/sb4_api_versioning/Sb4ApiVersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ganeshpandey.sb4_api_versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb4ApiVersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
